4ddbea4737b104ac4d973f3ef189958fc9bdef59
wangkang <424643374@qq.com>
2022-08-05 11:05:03 +0800

    1

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -10,6 +10,6 @@ ts.set_token('bea67662261d745b6c338c30abd05e8b8d7d59798b566fd6fa5024ff')
 
 pro = ts.pro_api()
 
-df = pro.daily(ts_code='600141.SH', start_date='20220701', end_date='20220714')
+df = pro.daily(ts_code='600141.SH', start_date='20220726', end_date='20220727')
 
 print(df)
